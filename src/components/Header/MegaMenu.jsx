b63da742af4ac7691166200dbd2c5af9f559eacc
Naveen Kharwar <naveenkharwar21@protonmail.com>
2024-03-07 20:48:09 +0530

    Enhancement: add more element to the mega menu with the image logics

--- src/components/Header/MegaMenu.jsx
+++ src/components/Header/MegaMenu.jsx
@@ -6,8 +6,13 @@ import { useDatabase } from "../../contexts/DatabaseContext";
 const MegaMenu = () => {
   const { data } = useDatabase();
 
-  const { isOpen, allCategories, activeCategory, setActiveCategory } =
-    useHeader();
+  const {
+    isOpen,
+    allCategories,
+    activeCategory,
+    setActiveCategory,
+    showMegamenu,
+  } = useHeader();
 
   const [activeSubmenu, setActiveSubmenu] = useState();
   const [firstItem, setFirstItem] = useState(null);
@@ -34,64 +39,74 @@ const MegaMenu = () => {
   return (
     <div
       id="submenu-container"
-      className={`container mx-auto  pt-4 transition duration-500 ease-in-out ${isOpen ? "opacity-1 h-full -translate-y-4" : "h-0 opacity-0"}`}
+      className={`w-full transition duration-500 ease-in-out ${isOpen ? "h-full -translate-y-0 opacity-100" : "h-0 translate-y-3 opacity-0 "}`}
     >
-      <div className="flex gap-8">
-        <div className="flex-1 ">
-          <ul className="mt-10 flex justify-between border-b">
-            {allCategories.map((category) => (
-              <li
-                onClick={() => menuHandler(category)}
-                key={category}
-                className={`border-primary cursor-pointer pb-5 ${category === activeCategory ? "submenu-active border-b-2" : ""}`}
-              >
-                <span className="text-lg font-semibold capitalize">
-                  {category}
-                </span>
-              </li>
-            ))}
-          </ul>
-          {activeCategory && (
-            <ul className="my-4 grid grid-cols-2 grid-rows-1 gap-3">
-              {data
-                .filter((item) => item.category === activeCategory)
-                .map((item) => (
-                  <li key={item.id} className="mb-3 flex items-center gap-5">
-                    <a href="#" className="flex items-center gap-5">
-                      <img
-                        src={item.image}
-                        alt={item.title}
-                        className="aspect-square w-12"
-                      />
-                      <h3 className="text-sm font-normal">{item.title}</h3>
-                    </a>
+      {showMegamenu}
+      <div>
+        <div
+          className={`absolute top-full w-full  ${isOpen ? "h-full" : "h-0"}`}
+        >
+          <div className="flex gap-8 bg-white p-10 pt-0">
+            <div className="flex-1 ">
+              <ul className="mt-10 flex justify-between border-b">
+                {allCategories.map((category) => (
+                  <li
+                    onClick={() => menuHandler(category)}
+                    key={category}
+                    className={`border-primary cursor-pointer pb-5 ${category === activeCategory ? "submenu-active border-b-2" : ""}`}
+                  >
+                    <span className="text-lg font-semibold capitalize">
+                      {category}
+                    </span>
                   </li>
                 ))}
-            </ul>
-          )}
-        </div>
-        <div className="border-primary relative mt-10 w-1/3 border-l-2 border-r-2 border-t-2">
-          <div className="absolute -top-4 left-1/2 -translate-x-1/2 transform bg-sky-700 px-2 py-1 text-white">
-            SALE
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <span className="my-10 block text-xl font-bold capitalize text-sky-700">
-              30% off on &#8201;
-              {activeCategory}
-            </span>
-            <div className="bg-transparent">
-              <img
-                src={firstItem}
-                alt="on-sale-product"
-                className="aspect-square h-60 w-60 mix-blend-multiply"
-              />
+              </ul>
+              {activeCategory && (
+                <ul className="my-4 grid grid-cols-2 grid-rows-1 gap-3">
+                  {data
+                    .filter((item) => item.category === activeCategory)
+                    .map((item) => (
+                      <li
+                        key={item.id}
+                        className="mb-3 flex items-center gap-5"
+                      >
+                        <a href="#" className="flex items-center gap-5">
+                          <img
+                            src={item.image}
+                            alt={item.title}
+                            className="aspect-square w-12"
+                          />
+                          <h3 className="text-sm font-normal">{item.title}</h3>
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+              )}
             </div>
-            <a
-              href="#"
-              className="mt-10 w-full bg-sky-700 py-4 text-center font-semibold uppercase text-white"
-            >
-              explore more
-            </a>
+            <div className="border-primary relative mt-10 w-1/3 border-l-2 border-r-2 border-t-2">
+              <div className="absolute -top-4 left-1/2 -translate-x-1/2 transform bg-sky-700 px-2 py-1 text-white">
+                SALE
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <span className="my-10 block text-xl font-bold capitalize text-sky-700">
+                  30% off on &#8201;
+                  {activeCategory}
+                </span>
+                <div className="bg-transparent">
+                  <img
+                    src={firstItem}
+                    alt="on-sale-product"
+                    className="aspect-square h-60 w-60 mix-blend-multiply"
+                  />
+                </div>
+                <a
+                  href="#"
+                  className="mt-10 w-full bg-sky-700 py-4 text-center font-semibold uppercase text-white"
+                >
+                  explore more
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>

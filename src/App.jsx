e7f0764778d0b7c90599d2038490720163ee669e
Naveen Kharwar <naveenkharwar21@protonmail.com>
2024-03-09 06:24:57 +0530

    Include: Hero section to the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 
 import { DatabaseProvider } from "./contexts/DatabaseContext.jsx";
 import { HeaderProvider } from "./contexts/HeaderContext.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 
 function App() {
   return (
     <DatabaseProvider>
       <HeaderProvider>
         <Header />
+        <Hero />
       </HeaderProvider>
     </DatabaseProvider>
   );
